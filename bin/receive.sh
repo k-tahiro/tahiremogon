2972fb0005e54389338284019a094ec0b3f686a9
Keisuke Hirota <tahiro.k.ad@gmail.com>
2018-07-16 14:10:26 +0900

    コマンドに改行が入らないように修正

--- bin/receive.sh
+++ bin/receive.sh
@@ -4,4 +4,4 @@ result="$(sudo /usr/local/bin/bto_ir_cmd -e -r)"
 if [ $? -ne 0 ]; then
   exit 1
 fi
-echo "${result}" | tail -n 1 | cut -f 2 -d : | cut -b 2-
+echo "${result}" | tail -n 1 | cut -f 2 -d : | cut -b 2- | tr -d '\n'
