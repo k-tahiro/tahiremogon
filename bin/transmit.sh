a69cc4a735c249939df4b13031bde0159cf199c3
k-tahiro <tahiro.k.ad@gmail.com>
2019-02-05 03:16:39 +0900

    Add log

--- bin/transmit.sh
+++ bin/transmit.sh
@@ -3,7 +3,8 @@
 readonly LOG_DIR="/var/log/tahiremogon"
 readonly CMD_LOG_FILE="${LOG_DIR}/cmd.log"
 
-sudo /usr/local/bin/bto_ir_cmd -e -t "$1" >>"${CMD_LOG_FILE}" 2>&1
+sudo /usr/local/bin/bto_ir_cmd -e -t "$1" >>"${CMD_LOG_FILE}" 2>&1 >>"${CMD_LOG_FILE}"
 FILE=$(camera.sh "${IMG_DIR}")
-mv "${FILE}" "/mnt/nasne/share2/tahiremocon/"
+echo "${FILE}" >>"${CMD_LOG_FILE}"
+mv "${FILE}" "/mnt/nasne/share2/tahiremocon/" >>"${CMD_LOG_FILE}"
 curl "192.168.10.43:5042/detect/$(basename "${FILE}")" | python -c 'import json; import sys; d = json.loads(raw_input()); print(d["status"])' | tr -d '\n'
