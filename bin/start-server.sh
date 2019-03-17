93e69bc550bc81ed0182f63540c80119c8ca7e59
k-tahiro <tahiro.k.ad@gmail.com>
2019-03-18 00:40:26 +0900

    Fix path

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 
 . "${SCRIPT_DIR}/../config/conf.txt"
+DB_FILE="${1:-"${SCRIPT_DIR}/../command.db"}"
 
 pushd "${SCRIPT_DIR}/../server"
 docker build -t tahiro/tahiremogon .
