d6d5a197ae32d1c54b6d48ec8e87e6a636069a4c
k-tahiro <tahiro.k.ad@gmail.com>
2019-03-18 00:44:19 +0900

    Bug fix

--- bin/ngrok.sh
+++ bin/ngrok.sh
@@ -2,4 +2,4 @@
 
 nohup ngrok http 1323 -region=ap -log=stdout >ngrok.log &
 sleep 60
-curl -s localhost:4040/status | grep ngrok.io | cut -f 2,3,4 -d '(' | cut -f 1,2,3 -d ')' | python3 -c 'import json; print(json.loads(input())["Session"]["Tunnels"]["command_line"]["URL"])' | tr -d '\n'
+eval echo "$(curl -s localhost:4040/status | grep ngrok.io | cut -f 2,3,4 -d '(' | cut -f 1,2,3 -d ')')" | python3 -c 'import json; print(json.loads(input())["Session"]["Tunnels"]["command_line"]["URL"])' | tr -d '\n'
